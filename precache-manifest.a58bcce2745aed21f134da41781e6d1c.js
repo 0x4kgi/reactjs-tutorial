af9a6dd28cf442a21f01399824d300b3ad3fff01
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2019-11-13 08:32:22 +0800

    Updates

---
+++ precache-manifest.a58bcce2745aed21f134da41781e6d1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c82313c9ce8856a3e81e08f45ca9cc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d5c83c948f5488ef1432",
+    "url": "/my-app/static/css/main.6ccb8c33.chunk.css"
+  },
+  {
+    "revision": "eafa6d8e72beb6fdbdbc",
+    "url": "/my-app/static/js/2.95f92227.chunk.js"
+  },
+  {
+    "revision": "d5c83c948f5488ef1432",
+    "url": "/my-app/static/js/main.7015bded.chunk.js"
+  },
+  {
+    "revision": "247eec36cf986955c323",
+    "url": "/my-app/static/js/runtime-main.fe96fb05.js"
+  }
+]);
