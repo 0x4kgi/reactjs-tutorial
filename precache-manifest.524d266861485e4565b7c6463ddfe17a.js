4869f2b1dcc951bfa5bd1ae54749a1f66ccfdfd0
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2019-11-13 09:24:04 +0800

    Updates

---
+++ precache-manifest.524d266861485e4565b7c6463ddfe17a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b690b987f5e1d48fe0aec02fb3efaceb",
+    "url": "/reactjs-tutorial/index.html"
+  },
+  {
+    "revision": "17de05ed2382959dcc75",
+    "url": "/reactjs-tutorial/static/css/main.b7f8c624.chunk.css"
+  },
+  {
+    "revision": "d4b1d08f33a52492ca0e",
+    "url": "/reactjs-tutorial/static/js/2.dc9fdf45.chunk.js"
+  },
+  {
+    "revision": "17de05ed2382959dcc75",
+    "url": "/reactjs-tutorial/static/js/main.be5072a0.chunk.js"
+  },
+  {
+    "revision": "7e3c600499b96f607435",
+    "url": "/reactjs-tutorial/static/js/runtime-main.dfc45483.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactjs-tictactoe/precache-manifest.a5adebc7e287bf049297af13f23a384b.js"
+  "/reactjs-tutorial/precache-manifest.524d266861485e4565b7c6463ddfe17a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactjs-tictactoe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactjs-tutorial/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
